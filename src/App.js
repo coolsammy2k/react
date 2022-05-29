00af0e4b5b485024904efeea3d91978c684f4fbf
Mohammad Samiullah <sam@MacBook-Pro.local>
2022-05-29 15:01:06 +0530

    movie api logic

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import React from 'react';
-import BackwardCounter from './components/BackwardCounter';
-import ForwardCounter from './components/ForwardCounter';
+
+import MoviesList from './components/MoviesList';
+import './App.css';
 
 function App() {
+  const dummyMovies = [
+    {
+      id: 1,
+      title: 'Some Dummy Movie',
+      openingText: 'This is the opening text of the movie',
+      releaseDate: '2021-05-18',
+    },
+    {
+      id: 2,
+      title: 'Some Dummy Movie 2',
+      openingText: 'This is the second opening text of the movie',
+      releaseDate: '2021-05-19',
+    },
+  ];
+
   return (
     <React.Fragment>
-      <ForwardCounter />
-      <BackwardCounter />
+      <section>
+        <button>Fetch Movies</button>
+      </section>
+      <section>
+        <MoviesList movies={dummyMovies} />
+      </section>
     </React.Fragment>
   );
 }
